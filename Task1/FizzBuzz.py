31f4c1ba5c2e628d7cbe4eab9a1cc026e1b724e8
Dmitry Yanovskiy <dyanych@gmail.com>
2022-09-23 14:06:10 +0300

    L1 - Algorithm FizzBuzz - optimazed

--- Task1/FizzBuzz.py
+++ Task1/FizzBuzz.py
@@ -1,11 +1,2 @@
 for i in range(1,101):
-    if i%15 == 0:
-        print('FizzBuzz', end='')
-    elif i%5 == 0:
-        print('Buzz', end='')
-    elif i%3 == 0:
-        print('Fizz', end='')
-    else:
-        print(i, end='')
-    print('', end=', ')
-    
+    print( _ if (_ := (i%3 == 0) * 'Fizz' + (i%5 == 0) * 'Buzz') else i, end=(i%100 != 0)*', ')
