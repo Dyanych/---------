d204867c358181f840364f10cb3a8760659a1c1b
Dmitry Yanovskiy <dyanych@gmail.com>
2022-09-28 12:44:46 +0300

    Added Palindrome algorithm

---
+++ GlobalTasks/Palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(x: int) -> bool:
+        if x < 0:
+            return False
+        elif x == 0:
+            return True
+        else:
+            pal = list()
+            while x:
+                pal.append(x%10)
+                x //= 10
+            for i in range(len(pal)//2):
+                if pal[i] != pal[-i-1]:
+                    return False
+            return True
+            
