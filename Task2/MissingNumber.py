c33d16aac493ead67baa1e94ab02970b679f9c78
Dmitry Yanovskiy <dyanych@gmail.com>
2022-10-03 10:31:21 +0300

    Change Alogorithm MissingNumber

--- Task2/MissingNumber.py
+++ Task2/MissingNumber.py
@@ -2,10 +2,11 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        sum = 0
-        for i in range (len(nums)):
-            sum += nums[i] - i - 1
-        return -1 * sum
+        sum, i = 0, 1
+        for x in nums:
+            sum += i - x
+            i +=1
+        return sum
 
 a = Solution()
 print(a.missingNumber([3, 0, 1]))
