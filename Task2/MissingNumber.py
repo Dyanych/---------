956da0c61a2569a904221e3ea76baaf3c025f2be
Dmitry Yanovskiy <dyanych@gmail.com>
2022-10-03 10:44:25 +0300

    Use len and sum function for MissingNumber

--- Task2/MissingNumber.py
+++ Task2/MissingNumber.py
@@ -2,11 +2,7 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        sum, i = 0, 1
-        for x in nums:
-            sum += i - x
-            i +=1
-        return sum
+        return ((2 * len(nums) + 1)**2 - 1)//8 - sum(nums) 
 
 a = Solution()
 print(a.missingNumber([3, 0, 1]))
