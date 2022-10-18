84eea393fb9aeca04cd2f0723718e730900ac248
Dmitry Yanovskiy <dyanych@gmail.com>
2022-10-18 22:11:09 +0300

    Change for get method

--- Task4/451._SortCharactersByFrequency.py
+++ Task4/451._SortCharactersByFrequency.py
@@ -3,8 +3,8 @@ class Solution:
         countSmb = dict()
         for smb in s:
             countSmb[smb] = countSmb.get(smb, 0) + 1
-        return ''.join([smb[0] * smb[1] for smb in \
-        sorted(countSmb.items(), key=lambda _: _[1], reverse=True)])
+        return ''.join(smb * countSmb[smb] for smb in \
+            sorted(countSmb, key=countSmb.get, reverse=True))
 
 
 a = Solution()
