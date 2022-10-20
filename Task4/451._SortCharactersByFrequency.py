cc0bb18c5af1d98bae565877848d4c9060cb8d85
Dmitry Yanovskiy <dyanych@gmail.com>
2022-10-20 09:36:22 +0300

    Sort Characters By Frequency Using Counter class

--- Task4/451._SortCharactersByFrequency.py
+++ Task4/451._SortCharactersByFrequency.py
@@ -1,11 +1,9 @@
+from typing import Counter
+
+
 class Solution:
     def frequencySort(self, s: str) -> str:
-        countSmb = dict()
-        for smb in s:
-            countSmb[smb] = countSmb.get(smb, 0) + 1
-        return ''.join(smb * countSmb[smb] for smb in \
-            sorted(countSmb, key=countSmb.get, reverse=True))
-
+        return ''.join([smb * n for smb, n in Counter(s).most_common()])
 
 a = Solution()
 print(a.frequencySort("Aabb"))
